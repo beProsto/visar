b4cd4bc90a43f586bfd11e270c805f628e814e47
beProsto <lavionperavion@gmail.com>
2022-10-09 23:35:49 +0200

    cmake: utility file

---
+++ cmake_include/utils.cmake
@@ -0,0 +1,48 @@
+if(WIN32)
+	set(VISAR_SYSTEM_WINDOWS TRUE)
+
+elseif(UNIX)
+	set(VISAR_SYSTEM_LINUX TRUE)
+
+endif()
+
+
+if(LIBTYPE STREQUAL "STATIC")
+	set(VISAR_BUILD_STATIC TRUE)
+
+else()
+	set(VISAR_BUILD_SHARED TRUE)
+
+endif()
+
+
+if(BUILD_EXAMPLES STREQUAL "NO")
+	set(VISAR_EXAMPLES_OFF TRUE)
+
+else()
+	set(VISAR_EXAMPLES_ON TRUE)
+
+endif()
+
+
+if(VISAR_SYSTEM_WINDOWS)
+	set(VISAR_EXE_TYPE WIN32)
+
+else()
+	set(VISAR_EXE_TYPE )
+
+endif()
+
+
+function(VISAR_ADD_EXAMPLE name)
+	file(GLOB_RECURSE ${name}_srcs
+		"examples/${name}/*.hpp"
+		"examples/${name}/*.cpp"
+	)
+
+	add_executable(${name} ${VISAR_EXE_TYPE} ${${name}_srcs})
+
+	if(VISAR_BUILD_STATIC)
+		target_compile_definitions(${name} PRIVATE VISAR_BUILD_STATIC)
+	endif()
+endfunction()
